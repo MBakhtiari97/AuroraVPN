3a04c40b996e04cadd5b6419274ad2be404a5b3c
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-03-08 17:57:41 +0330

    Fixing layout bugs

---
+++ AuroraVPN.DataLayer/Entities/Newsletter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraVPN.DataLayer.Entities
+{
+    public class Newsletter
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress,ErrorMessage = "Please insert a valid email address !")]
+        public string EmailAddress { get; set; }
+    }
+}
