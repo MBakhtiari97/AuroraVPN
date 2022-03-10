1bd10acc12023559fe245048c673c98fa9415b89
mohammadba86@yahoo.com <mohammadba86@yahoo.com>
2022-03-10 08:16:49 +0330

    Applying Payment Form

---
+++ AuroraVPN.Web/Controllers/PayController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AuroraVPN.Core.Generators;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuroraVPN.Web.Controllers
+{
+    public class PayController : Controller
+    {
+        [Route("MakePayment")]
+        public IActionResult Pay()
+        {
+            return View();
+        }
+
+        [Route("Payment")]
+        [HttpPost]
+        public IActionResult SuccessPayment()
+        {
+            ViewBag.Username = RandomStringGenerator.RandomString(8);
+            ViewBag.Password = RandomStringGenerator.RandomString(8);
+            return View();
+        }
+    }
+}
